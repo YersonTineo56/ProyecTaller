55beca3ec3255e4e52fb354592528e14adc6a171
YersonTineo56 <yeileon1410@gmail.com>
2024-01-08 15:13:50 -0500

    Initial commit

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/MicroserviciosHospedajeApplication.java
@@ -0,0 +1,13 @@
+package com.TallerYei.MicroserviciosHospedaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosHospedajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosHospedajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TallerYei/MicroserviciosHospedaje/MicroserviciosHospedajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TallerYei.MicroserviciosHospedaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosHospedajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
