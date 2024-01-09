2171afd446b4e198a3e369b77cf70ec5b075926d
YersonTineo56 <yeileon1410@gmail.com>
2024-01-09 10:48:41 -0500

    Create modelUsuario.java

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/model/modelUsuario.java
@@ -0,0 +1,48 @@
+package com.TallerYei.MicroserviciosHospedaje.model;
+
+public class modelUsuario {
+@Entity
+@Table(name="usuarios")
+public class modelUsuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idUSuario")
+    public int idUsuario;
+
+    @Column(name="usuario")
+    public String usuario;
+
+    @Column(name="clave")
+    public String clave;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    
+
+
+}
+
+
