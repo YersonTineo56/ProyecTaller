af9593c02b3d78b5f8fad7ff8c2a24c33eb180e4
YersonTineo56 <yeileon1410@gmail.com>
2024-01-09 11:01:22 -0500

    Update AuthRequest.java

--- src/main/java/com/TallerYei/MicroserviciosHospedaje/dto/AuthRequest.java
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/dto/AuthRequest.java
@@ -1,5 +1,35 @@
 package com.TallerYei.MicroserviciosHospedaje.dto;
 
-public class UsuarioRequest {
-    
+import java.io.Serializable;
+
+public class AuthRequest implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String usuario;
+    private String clave;
+ 
+    public AuthRequest() {
+ 
+    }
+ 
+    public AuthRequest(String usuario, String clave) {
+        this.setUsuario(usuario);
+        this.setClave(clave);
+    }
+ 
+    public String getClave() {
+        return clave;
+    }
+ 
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+ 
+    public String getUsuario() {
+        return usuario;
+    }
+ 
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
 }
