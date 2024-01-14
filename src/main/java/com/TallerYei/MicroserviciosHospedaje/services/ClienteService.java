510e49054e6ea671257ef1d567442c276d156f56
YersonTineo56 <yeileon1410@gmail.com>
2024-01-14 02:26:21 -0500

    Create ClienteService.java

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/services/ClienteService.java
@@ -0,0 +1,63 @@
+package com.TallerYei.MicroserviciosHospedaje.services;
+
+import com.TallerYei.MicroserviciosHospedaje.dto.ClienteDTO;
+import com.TallerYei.MicroserviciosHospedaje.model.Cliente;
+import com.TallerYei.MicroserviciosHospedaje.repository.IClienteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private IClienteRepository clienteRepository;
+    
+    public List<Cliente> obtenerTodosLosClientes() {
+        return (List<Cliente>) clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> obtenerClientePorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente crearCliente(ClienteDTO nuevoClienteDTO) {
+        Cliente nuevoCliente = convertirDTOaEntidad(nuevoClienteDTO);
+        return clienteRepository.save(nuevoCliente);
+    }
+
+    public Optional<Cliente> actualizarCliente(Long id, ClienteDTO clienteActualizadoDTO) {
+        return clienteRepository.findById(id).map(cliente -> {
+            actualizarEntidadDesdeDTO(cliente, clienteActualizadoDTO);
+            return clienteRepository.save(cliente);
+        });
+    }
+
+    public void eliminarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    // Métodos adicionales según tus necesidades
+
+    private Cliente convertirDTOaEntidad(ClienteDTO clienteDTO) {
+        return new Cliente(
+                clienteDTO.getNombre(),
+                clienteDTO.getApellido(),
+                clienteDTO.getDireccion(),
+                clienteDTO.getTelefono(),
+                clienteDTO.getCorreoElectronico()
+        );
+    }
+
+    private void actualizarEntidadDesdeDTO(Cliente cliente, ClienteDTO clienteDTO) {
+        cliente.setNombre(clienteDTO.getNombre());
+        cliente.setApellido(clienteDTO.getApellido());
+        cliente.setDireccion(clienteDTO.getDireccion());
+        cliente.setTelefono(clienteDTO.getTelefono());
+        cliente.setCorreoElectronico(clienteDTO.getCorreoElectronico());
+        // Puedes agregar más actualizaciones según tus necesidades
+    }
+}
