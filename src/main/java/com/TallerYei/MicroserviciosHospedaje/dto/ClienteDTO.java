7c97108898ced9b1446f406893028db435def8eb
YersonTineo56 <yeileon1410@gmail.com>
2024-01-14 13:15:56 -0500

    Update ClienteDTO.java

--- src/main/java/com/TallerYei/MicroserviciosHospedaje/dto/ClienteDTO.java
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/dto/ClienteDTO.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class ClienteDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     private String nombre;
     private String apellido;
     private String direccion;
