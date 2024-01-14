7a75ee75b7c51afc611c8383611d85330b1efb59
YersonTineo56 <yeileon1410@gmail.com>
2024-01-14 14:42:57 -0500

    Create Habitacion.java

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/model/Habitacion.java
@@ -0,0 +1,92 @@
+package com.TallerYei.MicroserviciosHospedaje.model;
+
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "habitacion")
+public class Habitacion implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "numerohabitacion")
+    private int numeroHabitacion;
+
+    @Column(name = "tipohabitacion")
+    private String tipoHabitacion;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @Column(name = "estado")
+    private String estado;
+
+    // Constructores, getters y setters
+
+    // Constructor por defecto
+    public Habitacion() {
+    }
+
+    // Constructor con todos los campos
+    public Habitacion(int numeroHabitacion, String tipoHabitacion, double precio, String estado) {
+        this.numeroHabitacion = numeroHabitacion;
+        this.tipoHabitacion = tipoHabitacion;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getNumeroHabitacion() {
+        return numeroHabitacion;
+    }
+
+    public void setNumeroHabitacion(int numeroHabitacion) {
+        this.numeroHabitacion = numeroHabitacion;
+    }
+
+    public String getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(String tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    // Otros métodos si es necesario
+}
