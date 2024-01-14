f20af41565775a02fe14d16eab4128443ff070ac
YersonTineo56 <yeileon1410@gmail.com>
2024-01-14 13:15:58 -0500

    Create Cliente.java

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/model/Cliente.java
@@ -0,0 +1,94 @@
+package com.TallerYei.MicroserviciosHospedaje.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ClienteID")
+    private Long clienteID;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "correoElectronico")
+    private String correoElectronico;
+
+    // Constructores, getters y setters
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellido, String direccion, String telefono, String correoElectronico) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public Long getClienteID() {
+        return clienteID;
+    }
+
+    public void setClienteID(Long clienteID) {
+        this.clienteID = clienteID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
