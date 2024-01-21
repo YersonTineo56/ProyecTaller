9b283d2fdb9c34c62788f0792d63649e56e5f288
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 17:40:32 -0500

    Update HabitacionService.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

--- src/main/java/com/TallerYei/MicroserviciosHospedaje/MicroservicioHabitacion/services/HabitacionService.java
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/MicroservicioHabitacion/services/HabitacionService.java
@@ -1,46 +1,66 @@
 package com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.services;
 
+import com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.dto.HabitacionDTO;
 import com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.model.Habitacion;
 import com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.repository.HabitacionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class HabitacionService {
 
+    private final HabitacionRepository habitacionRepository;
+
     @Autowired
-    private HabitacionRepository habitacionRepository;
-
-    public List<Habitacion> getAllHabitaciones() {
-        return (List<Habitacion>) habitacionRepository.findAll();
+    public HabitacionService(HabitacionRepository habitacionRepository) {
+        this.habitacionRepository = habitacionRepository;
     }
 
-    public Habitacion getHabitacionById(Long id) {
-        return habitacionRepository.findById(id).orElse(null);
+    public List<HabitacionDTO> getAllHabitaciones() {
+        Iterable<Habitacion> habitaciones = habitacionRepository.findAll();
+        return convertToDTOList(habitaciones);
     }
 
-    public Habitacion createHabitacion(Habitacion nuevaHabitacion) {
-        return habitacionRepository.save(nuevaHabitacion);
+    public HabitacionDTO getHabitacionById(Long id) {
+        Habitacion habitacion = habitacionRepository.findById(id).orElse(null);
+        return (habitacion != null) ? convertToDTO(habitacion) : null;
     }
 
-    public Habitacion updateHabitacion(Long id, Habitacion habitacionActualizada) {
-        Habitacion habitacionExistente = habitacionRepository.findById(id).orElse(null);
-
-        if (habitacionExistente != null) {
-            habitacionExistente.setNumeroHabitacion(habitacionActualizada.getNumeroHabitacion());
-            habitacionExistente.setTipoHabitacion(habitacionActualizada.getTipoHabitacion());
-            habitacionExistente.setPrecio(habitacionActualizada.getPrecio());
-            habitacionExistente.setOcupada(habitacionActualizada.isOcupada());
-
-            return habitacionRepository.save(habitacionExistente);
-        } else {
-            return null;
-        }
+    public HabitacionDTO createHabitacion(HabitacionDTO nuevaHabitacionDTO) {
+        Habitacion nuevaHabitacion = convertToEntity(nuevaHabitacionDTO);
+        Habitacion habitacionCreada = habitacionRepository.save(nuevaHabitacion);
+        return convertToDTO(habitacionCreada);
     }
 
     public void deleteHabitacion(Long id) {
         habitacionRepository.deleteById(id);
+    }
+
+    private List<HabitacionDTO> convertToDTOList(Iterable<Habitacion> habitaciones) {
+        return StreamSupport.stream(habitaciones.spliterator(), false)
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    private HabitacionDTO convertToDTO(Habitacion habitacion) {
+        return new HabitacionDTO(
+                habitacion.getNumeroHabitacion(),
+                habitacion.getTipoHabitacion(),
+                habitacion.getPrecio(),
+                habitacion.isOcupada()
+        );
+    }
+
+    private Habitacion convertToEntity(HabitacionDTO habitacionDTO) {
+        Habitacion entity = new Habitacion();
+        entity.setNumeroHabitacion(habitacionDTO.getNumeroHabitacion());
+        entity.setTipoHabitacion(habitacionDTO.getTipoHabitacion());
+        entity.setPrecio(habitacionDTO.getPrecio());
+        entity.setOcupada(habitacionDTO.isOcupada());
+        return entity;
     }
 }
