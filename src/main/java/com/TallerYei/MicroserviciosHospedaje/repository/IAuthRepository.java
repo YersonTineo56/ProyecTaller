acb5b599b81b02f943e7c25309e06a79fd3717c3
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 16:19:19 -0500

    Delete IAuthRepository.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

--- src/main/java/com/TallerYei/MicroserviciosHospedaje/repository/IAuthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.TallerYei.MicroserviciosHospedaje.repository;
-
-import com.TallerYei.MicroserviciosHospedaje.model.modelUsuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IAuthRepository extends JpaRepository<modelUsuario, Integer> {
-    // Puedes agregar métodos personalizados del repositorio si es necesario
-}
