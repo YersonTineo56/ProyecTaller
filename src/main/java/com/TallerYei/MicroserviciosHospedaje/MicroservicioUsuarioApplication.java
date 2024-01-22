4105075e48f9bc84e31b0f01754ad03345e7856e
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 22:31:41 -0500

    Create MicroservicioUsuarioApplication.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

---
+++ src/main/java/com/TallerYei/MicroserviciosHospedaje/MicroservicioUsuarioApplication.java
@@ -0,0 +1,12 @@
+package com.TallerYei.MicroserviciosHospedaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUsuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioUsuarioApplication.class, args);
+    }
+}
