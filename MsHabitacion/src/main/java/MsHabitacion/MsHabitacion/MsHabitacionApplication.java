feee70d2cd8a8cb45341833b7d451421f0ab94c0
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 22:31:19 -0500

    Create MsHabitacionApplication.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

---
+++ MsHabitacion/src/main/java/MsHabitacion/MsHabitacion/MsHabitacionApplication.java
@@ -0,0 +1,14 @@
+package MsHabitacion.MsHabitacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHabitacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHabitacionApplication.class, args);
+		System.err.println("corriendo");
+	}
+
+}
