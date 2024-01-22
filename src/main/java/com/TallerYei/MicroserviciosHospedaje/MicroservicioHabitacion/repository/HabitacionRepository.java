dee33c9719802e65a8b24dad5bfa4da7a1d6f6e4
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 23:07:53 -0500

    Delete HabitacionRepository.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

--- src/main/java/com/TallerYei/MicroserviciosHospedaje/MicroservicioHabitacion/repository/HabitacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.repository;
-
-
-import com.TallerYei.MicroserviciosHospedaje.MicroservicioHabitacion.model.Habitacion;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HabitacionRepository extends CrudRepository<Habitacion, Long> {
-    // Puedes agregar métodos de consulta específicos si los necesitas
-}
