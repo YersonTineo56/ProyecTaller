840ae68d37d75432d1a67fc665d5cbb533e20486
YersonTineo56 <yeileon1410@gmail.com>
2024-01-21 22:31:24 -0500

    Create MsHabitacionApplicationTests.java
    Co-Authored-By: TheMendez1664 <43259010+TheMendez1664@users.noreply.github.com>

---
+++ MsHabitacion/src/test/java/MsHabitacion/MsHabitacion/MsHabitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package MsHabitacion.MsHabitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHabitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
